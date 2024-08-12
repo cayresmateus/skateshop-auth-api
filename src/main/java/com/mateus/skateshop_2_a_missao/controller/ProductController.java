2553091d49a17fbe34787f3052871e8e4e4a70e3
mateus cayres <mateustfcayres@gmail.com>
2024-08-12 16:22:09 -0400

    feat: list and restock methods

---
+++ src/main/java/com/mateus/skateshop_2_a_missao/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.mateus.skateshop_2_a_missao.controller;
+
+import com.mateus.skateshop_2_a_missao.dto.ProductResponseDTO;
+import com.mateus.skateshop_2_a_missao.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> list(){
+        var products = service.findAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<ProductResponseDTO> findByName(@PathVariable String name){
+        var product = service.findByName(name);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping("/restock/{id}")
+    public void restock(@PathVariable Long id, @RequestBody Long quantity){
+        service.restock(id, quantity);
+    }
+
+
+}
