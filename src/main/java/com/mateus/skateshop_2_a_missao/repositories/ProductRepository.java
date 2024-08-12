4cbd6c05fb1481f2b5aee9ed16d77fcd7d02eea5
mateus cayres <mateustfcayres@gmail.com>
2024-08-12 16:23:55 -0400

    feat: product repository

---
+++ src/main/java/com/mateus/skateshop_2_a_missao/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.mateus.skateshop_2_a_missao.repositories;
+
+import com.mateus.skateshop_2_a_missao.domain.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+
+}
