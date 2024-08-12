3256ef47454283957fd9b4039b5c62a1499fe0e2
mateus cayres <mateustfcayres@gmail.com>
2024-08-12 16:23:15 -0400

    feat: product response dto

---
+++ src/main/java/com/mateus/skateshop_2_a_missao/dto/ProductResponseDTO.java
@@ -0,0 +1,18 @@
+package com.mateus.skateshop_2_a_missao.dto;
+
+import com.mateus.skateshop_2_a_missao.domain.product.Brand;
+import com.mateus.skateshop_2_a_missao.domain.product.Product;
+
+import java.math.BigDecimal;
+
+public record ProductResponseDTO(String name,
+                                 BigDecimal price,
+                                 String description,
+                                 Brand brand,
+                                 String imageUrl
+                                 ) {
+    public ProductResponseDTO(Product p){
+        this( p.getName(), p.getPrice(), p.getDescription(), p.getBrand(), p.getImageUrl());
+
+    }
+}
