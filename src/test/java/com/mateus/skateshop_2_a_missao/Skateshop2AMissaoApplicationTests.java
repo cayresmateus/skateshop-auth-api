24ee27b16a245228be4fdb84e42eb94edd88a89c
mateus cayres <mateustfcayres@gmail.com>
2024-08-12 16:22:19 -0400

    test

---
+++ src/test/java/com/mateus/skateshop_2_a_missao/Skateshop2AMissaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.skateshop_2_a_missao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skateshop2AMissaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
