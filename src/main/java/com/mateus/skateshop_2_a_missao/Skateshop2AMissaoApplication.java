3cad919ff338c8d39a1559d1f50811d02e357879
mateus cayres <mateustfcayres@gmail.com>
2024-08-12 16:22:36 -0400

    :tada:

---
+++ src/main/java/com/mateus/skateshop_2_a_missao/Skateshop2AMissaoApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.skateshop_2_a_missao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skateshop2AMissaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skateshop2AMissaoApplication.class, args);
+	}
+
+}
