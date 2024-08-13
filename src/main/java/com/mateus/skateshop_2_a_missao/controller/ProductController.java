f983363d324e01086367f4f909436f13a056dd7b
mateus cayres <mateustfcayres@gmail.com>
2024-08-13 16:44:08 -0400

    feat: create products endpoint

--- src/main/java/com/mateus/skateshop_2_a_missao/controller/ProductController.java
+++ src/main/java/com/mateus/skateshop_2_a_missao/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.mateus.skateshop_2_a_missao.controller;
 
+import com.mateus.skateshop_2_a_missao.dto.CreateProductDTO;
 import com.mateus.skateshop_2_a_missao.dto.ProductResponseDTO;
 import com.mateus.skateshop_2_a_missao.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,12 @@ public class ProductController {
     public ResponseEntity<ProductResponseDTO> findByName(@PathVariable String name){
         var product = service.findByName(name);
         return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createProduct(@RequestBody CreateProductDTO dto){
+        service.create(dto);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/restock/{id}")
