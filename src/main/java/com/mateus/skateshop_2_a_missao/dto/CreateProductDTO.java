57853b0b928fba57ec04942b4ac97ff066c71241
mateus cayres <mateustfcayres@gmail.com>
2024-08-13 16:42:58 -0400

    feat: CreateProductDTO

---
+++ src/main/java/com/mateus/skateshop_2_a_missao/dto/CreateProductDTO.java
@@ -0,0 +1,15 @@
+package com.mateus.skateshop_2_a_missao.dto;
+
+import com.mateus.skateshop_2_a_missao.domain.product.Brand;
+import com.mateus.skateshop_2_a_missao.domain.product.TypeProduct;
+
+import java.math.BigDecimal;
+
+public record CreateProductDTO(String name,
+                               BigDecimal price,
+                               String description,
+                               Brand brand,
+                               TypeProduct typeProduct,
+                               Long quantity,
+                               String imageUrl) {
+}
