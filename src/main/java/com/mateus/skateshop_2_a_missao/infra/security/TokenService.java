432a6db09f4f1430b5c57b2fe44f4b8496dae08a
mateus cayres <mateustfcayres@gmail.com>
2024-08-13 22:05:58 -0400

    feat: create token service

---
+++ src/main/java/com/mateus/skateshop_2_a_missao/infra/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.mateus.skateshop_2_a_missao.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.mateus.skateshop_2_a_missao.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${infra.security.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("skateshop-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpDate())
+                    .sign(algorithm);
+        }catch (JWTCreationException e){
+            throw new RuntimeException("Error while generating token", e);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("skateshop-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return "";
+        }
+    }
+
+    private Instant generateExpDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
