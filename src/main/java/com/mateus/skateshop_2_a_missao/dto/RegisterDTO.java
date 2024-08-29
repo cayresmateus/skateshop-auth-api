c6595a6f76e7bc0477855ed39d63d7df979b738d
cayresmateus <mateuscayres3@gmail.com>
2024-08-29 14:04:13 -0400

    refactor: removing UserRole from register dto

--- src/main/java/com/mateus/skateshop_2_a_missao/dto/RegisterDTO.java
+++ src/main/java/com/mateus/skateshop_2_a_missao/dto/RegisterDTO.java
@@ -3,5 +3,5 @@ package com.mateus.skateshop_2_a_missao.dto;
 
 import com.mateus.skateshop_2_a_missao.domain.user.UserRole;
 
-public record RegisterDTO(String username, String password, UserRole role) {
+public record RegisterDTO(String username, String password) {
 }
